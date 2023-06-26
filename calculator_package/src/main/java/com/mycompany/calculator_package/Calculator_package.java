84632880cfc7f0dfd02c5ab2e1e67fd49f768b29
Luyolo Pondo <109291978+Luyolop1@users.noreply.github.com>
2023-06-26 16:24:09 +0200

    Add files via upload

---
+++ calculator_package/src/main/java/com/mycompany/calculator_package/Calculator_package.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculator_package;
+
+/**
+ *
+ * @author Admin
+ */
+public class Calculator_package {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
